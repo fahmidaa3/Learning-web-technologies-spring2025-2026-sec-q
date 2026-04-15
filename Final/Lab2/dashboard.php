330d6b87e9308150c8ce2aaf0df7699c88e426ce
Fahmida Sultana <fahmidaumme33@gmail.com>
2026-04-15 14:37:17 +0600

    sixth push

---
+++ Final/Lab2/dashboard.php
@@ -0,0 +1,60 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['user'])) {
+    header("Location: login.php");
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Dashboard</title>
+</head>
+<body>
+
+<table border="1" width="70%" align="center" cellspacing="0" cellpadding="10">
+
+<tr>
+<td colspan="2">
+<b style="font-size:20px;">XCompany</b>
+<span style="float:right;">
+Logged in as <?php echo $_SESSION['user']['name']; ?> |
+<a href="logout.php">Logout</a>
+</span>
+</td>
+</tr>
+
+<tr>
+
+<td width="25%">
+<b>Account</b>
+<hr>
+
+<ul>
+<li><a href="dashboard.php">Dashboard</a></li>
+<li><a href="view_profile.php">View Profile</a></li>
+<li><a href="edit_profile.php">Edit Profile</a></li>
+<li><a href="change_picture.php">Change Profile Picture</a></li>
+<li><a href="change_password.php">Change Password</a></li>
+<li><a href="logout.php">Logout</a></li>
+</ul>
+</td>
+
+<td>
+<h3>Welcome <?php echo $_SESSION['user']['name']; ?></h3>
+</td>
+
+</tr>
+
+
+<tr>
+<td colspan="2" align="center">
+Copyright © 2017
+</td>
+</tr>
+
+</table>
+
+</body>
+</html>
