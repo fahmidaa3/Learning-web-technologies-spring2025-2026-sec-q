97a4c483a8a8b44a1166d0e364cb75bf666b7c76
Fahmida Sultana <fahmidaumme33@gmail.com>
2026-04-15 14:42:46 +0600

    login page updated

--- Final/Lab2/login.php
+++ Final/Lab2/login.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
 
+$msg = "";
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $username = $_POST['username'];
@@ -11,23 +13,98 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         $_SESSION['user'] = $_SESSION['users'][$username];
 
+        // Remember Me (Cookie)
         if (isset($_POST['remember'])) {
             setcookie("username", $username, time()+3600);
         }
 
         header("Location: dashboard.php");
+
     } else {
-        echo "Invalid login!";
+        $msg = "Invalid Username or Password!";
     }
 }
 ?>
 
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+
+<table border="1" width="60%" align="center" cellspacing="0" cellpadding="10">
+
+<tr>
+<td>
+<b style="font-size:20px;">XCompany</b>
+<span style="float:right;">
+<a href="index.php">Home</a> |
+<a href="login.php">Login</a> |
+<a href="registration.php">Registration</a>
+</span>
+</td>
+</tr>
+
+<tr>
+<td align="center">
+
 <form method="post">
-Username: <input name="username"><br>
-Password: <input type="password" name="password"><br>
+<fieldset style="width:350px;">
+<legend><b>LOGIN</b></legend>
 
-<input type="checkbox" name="remember"> Remember Me<br>
+<table>
 
-<input type="submit">
+<tr>
+<td>User Name</td>
+<td>: <input type="text" name="username"></td>
+</tr>
+
+<tr><td colspan="2"><hr></td></tr>
+
+<tr>
+<td>Password</td>
+<td>: <input type="password" name="password"></td>
+</tr>
+
+<tr><td colspan="2"><hr></td></tr>
+
+<tr>
+<td colspan="2">
+<input type="checkbox" name="remember"> Remember Me
+</td>
+</tr>
+
+<tr><td colspan="2"><hr></td></tr>
+
+<tr>
+<td colspan="2">
+<input type="submit" value="Submit">
 <a href="forgot_password.php">Forgot Password?</a>
+</td>
+</tr>
+
+<tr>
+<td colspan="2" align="center">
+<?php echo $msg; ?>
+</td>
+</tr>
+
+</table>
+
+</fieldset>
 </form>
+
+</td>
+</tr>
+
+<tr>
+<td align="center">
+Copyright © 2017
+</td>
+</tr>
+
+</table>
+
+</body>
+</html>
