12d8af172039a7c4ef4034e28df775a344ac957f
Fahmida Sultana <fahmidaumme33@gmail.com>
2026-04-15 13:56:28 +0600

    first push

---
+++ Final/Lab2/index.php
@@ -0,0 +1,9 @@
+<?php session_start(); ?>
+
+<h2>Welcome to XCompany</h2>
+
+<a href="index.php">Home</a> |
+<a href="login.php">Login</a> |
+<a href="registration.php">Registration</a>
+
+<?php include("footer.php"); ?>
